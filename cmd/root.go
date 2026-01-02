92730ffaffb281f2fbdb6dab84d95c6e81ed6291
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2026-01-01 17:12:23 -0700

    Some notes for planning

--- cmd/root.go
+++ cmd/root.go
@@ -28,5 +28,16 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// `new` creates a new project
+	// `add` adds a file to an existing project
+	// `config.global` edits the global config
+	// `config.local` edits the config for a project
+	// `list templates` lists the local templates available
+	// `list [template] files` lists the files you can add to a template
+	// long term a way to search for install, and remove templates would be good.
+	// want a `add` to add files to aproject
+	// figure out how to do templating of file names. Just use go?
+	// figure out how to support scripting: just one script that runs after the template step
+	// want to use lua for that.
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
