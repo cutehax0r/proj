b77b6c71da8c345c1a7cad3483bc28eb3b2d94dd
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2026-02-12 16:06:52 -0700

    add test

---
+++ tests/acceptance/add_test.go
@@ -0,0 +1,46 @@
+//go:build acceptance
+
+package acceptance
+
+import (
+	"testing"
+)
+
+func TestAddCommand_ShowsHelp(t *testing.T) {
+	ctx := Setup(t)
+
+	ctx.Run("add", "--help").
+		ExpectOutput("Usage:").
+		ExpectOutput("proj add <kind> <name> [flags]").
+		ExpectOutput("Flags:").
+		ExpectOutput("--target-path").
+		ExpectOutput("--template-path").
+		ExpectOutput("Global Flags:")
+}
+
+func TestAddCommand_RequiresArgs(t *testing.T) {
+	ctx := Setup(t)
+
+	ctx.Run("add").
+		ExpectError("Usage:").
+		ExpectError("proj add <kind> <name> [flags]").
+		ExpectExitCode(1)
+}
+
+func TestAddCommand_RequiresTwoArgs(t *testing.T) {
+	ctx := Setup(t)
+
+	ctx.Run("add", "foo").
+		ExpectError("accepts 2 arg(s), received 1").
+		ExpectError("Usage:").
+		ExpectExitCode(1)
+}
+
+func TestAddCommand_FailsWhenNotInProjDirectory(t *testing.T) {
+	ctx := Setup(t)
+
+	ctx.Run("add", "foo", "bar").
+		ExpectOutput("Failed to setup configuration").
+		ExpectOutput("not in a proj directory").
+		ExpectExitCode(1)
+}
