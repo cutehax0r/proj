e57da12326a4102389fa941d4169ca6413bce63e
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2026-03-20 13:01:21 -0600

    use new handlers, plan for extracting setup

--- internal/generator/adder.go
+++ internal/generator/adder.go
@@ -1,17 +1,13 @@
 package generator
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
 	"log/slog"
 	"path/filepath"
 	"proj/internal/config"
 	"proj/internal/luabridge"
 	"proj/internal/paths"
 	"strings"
-	"text/template"
 
 	"github.com/spf13/afero"
 	"github.com/spf13/viper"
@@ -119,68 +115,30 @@ func (a *Adder) setupConfig() error {
 		return afero.ErrFileNotFound
 	}
 
-	reqs, err := config.NewRequirements()
+	reqs, err := loadRequirements()
 	if err != nil {
-		slog.Error("Failed to load requirements", slog.Any("error", err))
 		return err
 	}
 	a.reqs = reqs
-	slog.Debug("Final Requirements", slog.Any("reqs", reqs))
 
-	if !reqs.Local {
-		slog.Error("Cannot use non-local definition to add to existing project", slog.String("definition-name", a.cfg.DefinitionName))
-		return errors.New("cannot use non-local definition to add to existing project")
-	}
-
-	vars, err := config.BuildVariables(reqs.Variables)
+	vars, scripts, files, luaenv, err := loadAdderState(a.cfg, a.paths, a.reqs)
 	if err != nil {
-		slog.Error("Failed to build variables", slog.Any("error", err))
 		return err
 	}
 	a.vars = vars
-	slog.Debug("Final Variables", slog.Any("vars", vars))
-
-	scripts, err := config.NewScriptSpecWithFS(a.cfg.Fs, a.paths)
-	if err != nil {
-		slog.Error("Couldn't build scripts", slog.Any("error", err))
-		return err
-	}
 	a.scripts = scripts
-	slog.Debug("Final scripts", slog.Any("scripts", scripts))
-
-	files, err := config.NewFileSpecsWithFS(a.cfg.Fs, a.paths)
-	if err != nil {
-		slog.Error("Failed to load files from template definition", slog.Any("error", err))
-		return err
-	}
 	a.files = files
-	slog.Debug("Final files", slog.Any("files", files))
-
-	a.luaenv = luabridge.NewRuntime(a.vars, a.paths, a.reqs, &a.files, a.cfg.NoWrite)
+	a.luaenv = luaenv
 
 	return nil
 }
 
 func (a *Adder) runBeforeScripts() error {
-	for _, script := range a.scripts.BeforeScriptsWithFS(a.cfg.Fs) {
-		if err := a.luaenv.Run(script); err != nil {
-			slog.Error("Error in lua script. Aborting", slog.Any("error", err), slog.String("script", script))
-			return err
-		}
+	vars, err := runBeforeScriptsAndValidateVars(a.luaenv, a.scripts.BeforeScriptsWithFS(a.cfg.Fs), a.reqs)
+	if err != nil {
+		return err
 	}
-
-	a.vars = a.luaenv.GetVariables()
-	slog.Debug("Variables after before-scripts", slog.Any("vars", a.vars))
-
-	for _, varspec := range a.reqs.Variables {
-		normalizedName := strings.ToLower(varspec.Name)
-		if a.vars[normalizedName] == nil {
-			slog.Error("Required variable is not set. Use --set-variable. Aborting.", slog.Any("Name", varspec.Name))
-			slog.Info("All variables", slog.Any("vars", a.vars))
-			return errors.New("required variable not set")
-		}
-	}
-	slog.Debug("All the variables are ready so we can do the work")
+	a.vars = vars
 
 	return nil
 }
@@ -190,7 +148,7 @@ func (a *Adder) validateFiles() error {
 
 	for _, file := range a.files {
 		// Render the destination filename
-		destPath, err := a.renderTargetPath(file.Target)
+		destPath, err := renderTargetPath(file.Target, a.vars)
 		if err != nil {
 			slog.Error("Error templating target filename", slog.Any("error", err), slog.String("target", file.Target))
 			return err
@@ -214,142 +172,9 @@ func (a *Adder) validateFiles() error {
 }
 
 func (a *Adder) processFiles() error {
-	for i, file := range a.files {
-		destPath, err := a.renderTargetPath(file.Target)
-		if err != nil {
-			slog.Error("Error templating target filename", slog.Any("error", err), slog.String("target", file.Target))
-			return err
-		}
-
-		if file.Parse {
-			if err := a.renderAndWriteFile(i, destPath); err != nil {
-				return err
-			}
-		} else {
-			if err := a.copyFile(i, destPath); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (a *Adder) renderTargetPath(targetTemplate string) (string, error) {
-	desttemp, err := template.New("filename").Parse(targetTemplate)
-	if err != nil {
-		slog.Error("Couldn't template the target filename", slog.String("target", targetTemplate), slog.Any("err", err))
-		return "", err
-	}
-
-	var deststr bytes.Buffer
-	err = desttemp.Execute(&deststr, a.vars)
-	if err != nil {
-		slog.Error("Error templating target filename", slog.Any("error", err), slog.String("target", targetTemplate))
-		return "", err
-	}
-
-	return deststr.String(), nil
-}
-
-func (a *Adder) renderAndWriteFile(fileIdx int, destPath string) error {
-	file := a.files[fileIdx]
-
-	slog.Info("parsing content of file", slog.String("source", file.Source), slog.String("target", destPath))
-
-	rawtemp, err := afero.ReadFile(a.cfg.Fs, file.Source)
-	if err != nil {
-		slog.Error("Couldn't read the raw template data", slog.String("Source", file.Source), slog.Any("err", err))
-		return err
-	}
-	file.Raw = string(rawtemp)
-	slog.Debug("rendering template", slog.String("raw data", file.Raw))
-
-	conttemp, err := template.New("template").Parse(file.Raw)
-	if err != nil {
-		slog.Error("Error parsing template", slog.Any("err", err), slog.Any("file", file.Source), slog.Any("paths", a.paths))
-		return err
-	}
-
-	var contbuff bytes.Buffer
-	if err := conttemp.Execute(&contbuff, a.vars); err != nil {
-		slog.Error("Error executing template", slog.Any("error", err))
-		return err
-	}
-	file.Rendered = contbuff.String()
-	slog.Info("result", slog.String("rendered", file.Rendered))
-
-	if a.cfg.NoWrite {
-		slog.Debug("No-write set: skipping write", slog.String("source", file.Source), slog.String("target", destPath))
-		return nil
-	}
-
-	targetDir := filepath.Dir(destPath)
-	if err := a.cfg.Fs.MkdirAll(targetDir, 0755); err != nil {
-		slog.Error("Failed to create target directory", slog.String("directory", targetDir), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Created target directory", slog.String("directory", targetDir))
-
-	if err := afero.WriteFile(a.cfg.Fs, destPath, []byte(file.Rendered), file.SourceMode); err != nil {
-		slog.Error("Failed to write rendered file", slog.String("target", destPath), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Wrote rendered file", slog.String("target", destPath))
-
-	return nil
-}
-
-func (a *Adder) copyFile(fileIdx int, destPath string) error {
-	file := a.files[fileIdx]
-
-	slog.Info("Nothing to render, skipping parse.", slog.String("source", file.Source), slog.String("target", destPath))
-
-	if a.cfg.NoWrite {
-		slog.Debug("No-write set: skipping copy", slog.String("source", file.Source), slog.String("target", destPath))
-		return nil
-	}
-
-	targetDir := filepath.Dir(destPath)
-	if err := a.cfg.Fs.MkdirAll(targetDir, 0755); err != nil {
-		slog.Error("Failed to create target directory", slog.String("directory", targetDir), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Created target directory", slog.String("directory", targetDir))
-
-	sourceFile, err := a.cfg.Fs.Open(file.Source)
-	if err != nil {
-		slog.Error("Failed to open source file", slog.String("source", file.Source), slog.Any("error", err))
-		return err
-	}
-	defer sourceFile.Close()
-
-	targetFile, err := a.cfg.Fs.Create(destPath)
-	if err != nil {
-		slog.Error("Failed to create target file", slog.String("target", destPath), slog.Any("error", err))
-		return err
-	}
-	defer targetFile.Close()
-
-	if _, err := io.Copy(targetFile, sourceFile); err != nil {
-		slog.Error("Failed to copy file", slog.String("source", file.Source), slog.String("target", destPath), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Copied file", slog.String("source", file.Source), slog.String("target", destPath))
-
-	if err := a.cfg.Fs.Chmod(destPath, file.SourceMode); err != nil {
-		slog.Warn("Failed to set permissions on copied file", slog.String("target", destPath), slog.Any("error", err))
-	}
-	slog.Debug("Set permissions on copied file", slog.String("target", destPath), slog.Any("mode", file.SourceMode))
-
-	return nil
+	return processFiles(a.cfg, a.paths, a.files, a.vars)
 }
 
 func (a *Adder) runAfterScripts() error {
-	for _, script := range a.scripts.AfterScriptsWithFS(a.cfg.Fs) {
-		if err := a.luaenv.Run(script); err != nil {
-			slog.Error("Error in lua script. Aborting", slog.Any("error", err), slog.String("script", script))
-			return err
-		}
-	}
-	return nil
+	return runAfterScripts(a.luaenv, a.scripts.AfterScriptsWithFS(a.cfg.Fs))
 }
--- internal/generator/creator.go
+++ internal/generator/creator.go
@@ -1,16 +1,12 @@
 package generator
 
 import (
-	"bytes"
-	"errors"
-	"io"
 	"log/slog"
 	"path/filepath"
 	"proj/internal/config"
 	"proj/internal/luabridge"
 	"proj/internal/paths"
 	"strings"
-	"text/template"
 
 	"github.com/spf13/afero"
 	"github.com/spf13/viper"
@@ -111,215 +107,38 @@ func (c *Creator) setupConfig() error {
 		return afero.ErrFileNotFound
 	}
 
-	reqs, err := config.NewRequirements()
+	reqs, err := loadRequirements()
 	if err != nil {
-		slog.Error("Failed to load requirements", slog.Any("error", err))
 		return err
 	}
 	c.reqs = reqs
-	slog.Debug("Final Requirements", slog.Any("reqs", reqs))
 
-	if reqs.Local {
-		slog.Error("cannot use local-only definition to create new project", slog.String("definition-name", c.cfg.DefinitionName))
-		return errors.New("cannot use local-only definition to create new project")
-	}
-
-	vars, err := config.BuildVariables(reqs.Variables)
+	vars, scripts, files, luaenv, err := loadCreatorState(c.cfg, c.paths, c.reqs)
 	if err != nil {
-		slog.Error("Failed to build variables", slog.Any("error", err))
 		return err
 	}
 	c.vars = vars
-	slog.Debug("Final Variables", slog.Any("vars", vars))
-
-	scripts, err := config.NewScriptSpecWithFS(c.cfg.Fs, c.paths)
-	if err != nil {
-		slog.Error("Couldn't build scripts", slog.Any("error", err))
-		return err
-	}
 	c.scripts = scripts
-	slog.Debug("Final scripts", slog.Any("scripts", scripts))
-
-	files, err := config.NewFileSpecsWithFS(c.cfg.Fs, c.paths)
-	if err != nil {
-		slog.Error("Failed to load files from template definition", slog.Any("error", err))
-		return err
-	}
 	c.files = files
-	slog.Debug("Final files", slog.Any("files", files))
-
-	c.luaenv = luabridge.NewRuntime(c.vars, c.paths, c.reqs, &c.files, c.cfg.NoWrite)
+	c.luaenv = luaenv
 
 	return nil
 }
 
 func (c *Creator) runBeforeScripts() error {
-	for _, script := range c.scripts.BeforeScriptsWithFS(c.cfg.Fs) {
-		if err := c.luaenv.Run(script); err != nil {
-			slog.Error("Error in lua script. Aborting", slog.Any("error", err), slog.String("script", script))
-			return err
-		}
+	vars, err := runBeforeScriptsAndValidateVars(c.luaenv, c.scripts.BeforeScriptsWithFS(c.cfg.Fs), c.reqs)
+	if err != nil {
+		return err
 	}
-
-	c.vars = c.luaenv.GetVariables()
-	slog.Debug("Variables after before-scripts", slog.Any("vars", c.vars))
-
-	for _, varspec := range c.reqs.Variables {
-		normalizedName := strings.ToLower(varspec.Name)
-		if c.vars[normalizedName] == nil {
-			slog.Error("Required variable is not set. Use --set-variable. Aborting.", slog.Any("Name", varspec.Name))
-			slog.Info("All variables", slog.Any("vars", c.vars))
-			return errors.New("required variable not set")
-		}
-	}
-	slog.Debug("All the variables are ready so we can do the work")
+	c.vars = vars
 
 	return nil
 }
 
 func (c *Creator) runAfterScripts() error {
-	for _, script := range c.scripts.AfterScriptsWithFS(c.cfg.Fs) {
-		if err := c.luaenv.Run(script); err != nil {
-			slog.Error("Error in lua script. Aborting", slog.Any("error", err), slog.String("script", script))
-			return err
-		}
-	}
-	return nil
+	return runAfterScripts(c.luaenv, c.scripts.AfterScriptsWithFS(c.cfg.Fs))
 }
 
 func (c *Creator) processFiles() error {
-	// We're going to read all of the files that we're going to parse into memory. Two copies
-	// (before and after parsing). That's not very ram efficient but it lets us do some nice
-	// debugging with --no-write by forcing parsing to happen without writing. Computers have
-	// lots of ram and text files are small so we're not sweating it.
-	for i, file := range c.files {
-		// Render the destination filename
-		destPath, err := c.renderTargetPath(file.Target)
-		if err != nil {
-			slog.Error("Error templating target filename", slog.Any("error", err), slog.String("target", file.Target))
-			return err
-		}
-
-		// Render file content if needed
-		if file.Parse {
-			if err := c.renderAndWriteFile(i, destPath); err != nil {
-				return err
-			}
-		} else {
-			if err := c.copyFile(i, destPath); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (c *Creator) renderTargetPath(targetTemplate string) (string, error) {
-	desttemp, err := template.New("filename").Parse(targetTemplate)
-	if err != nil {
-		slog.Error("Couldn't template the target filename", slog.String("target", targetTemplate), slog.Any("err", err))
-		return "", err
-	}
-
-	var deststr bytes.Buffer
-	err = desttemp.Execute(&deststr, c.vars)
-	if err != nil {
-		slog.Error("Error templating target filename", slog.Any("error", err), slog.String("target", targetTemplate))
-		return "", err
-	}
-
-	return deststr.String(), nil
-}
-
-func (c *Creator) renderAndWriteFile(fileIdx int, destPath string) error {
-	file := c.files[fileIdx]
-
-	slog.Info("parsing content of file", slog.String("source", file.Source), slog.String("target", destPath))
-
-	rawtemp, err := afero.ReadFile(c.cfg.Fs, file.Source)
-	if err != nil {
-		slog.Error("Couldn't read the raw template data", slog.String("Source", file.Source), slog.Any("err", err))
-		return err
-	}
-	file.Raw = string(rawtemp)
-	slog.Debug("rendering template", slog.String("raw data", file.Raw))
-
-	conttemp, err := template.New("template").Parse(file.Raw)
-	if err != nil {
-		slog.Error("Error parsing template", slog.Any("err", err), slog.Any("file", file.Source), slog.Any("paths", c.paths))
-		return err
-	}
-
-	var contbuff bytes.Buffer
-	if err := conttemp.Execute(&contbuff, c.vars); err != nil {
-		slog.Error("Error executing template", slog.Any("error", err))
-		return err
-	}
-	file.Rendered = contbuff.String()
-	slog.Info("result", slog.String("rendered", file.Rendered))
-
-	if c.cfg.NoWrite {
-		slog.Debug("No-write set: skipping write", slog.String("source", file.Source), slog.String("target", destPath))
-		return nil
-	}
-
-	targetDir := filepath.Dir(destPath)
-	if err := c.cfg.Fs.MkdirAll(targetDir, 0755); err != nil {
-		slog.Error("Failed to create target directory", slog.String("directory", targetDir), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Created target directory", slog.String("directory", targetDir))
-
-	if err := afero.WriteFile(c.cfg.Fs, destPath, []byte(file.Rendered), file.SourceMode); err != nil {
-		slog.Error("Failed to write rendered file", slog.String("target", destPath), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Wrote rendered file", slog.String("target", destPath))
-
-	return nil
-}
-
-func (c *Creator) copyFile(fileIdx int, destPath string) error {
-	file := c.files[fileIdx]
-
-	slog.Info("Nothing to render, skipping parse.", slog.String("source", file.Source), slog.String("target", destPath))
-
-	if c.cfg.NoWrite {
-		slog.Debug("No-write set: skipping copy", slog.String("source", file.Source), slog.String("target", destPath))
-		return nil
-	}
-
-	targetDir := filepath.Dir(destPath)
-	if err := c.cfg.Fs.MkdirAll(targetDir, 0755); err != nil {
-		slog.Error("Failed to create target directory", slog.String("directory", targetDir), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Created target directory", slog.String("directory", targetDir))
-
-	sourceFile, err := c.cfg.Fs.Open(file.Source)
-	if err != nil {
-		slog.Error("Failed to open source file", slog.String("source", file.Source), slog.Any("error", err))
-		return err
-	}
-	defer sourceFile.Close()
-
-	targetFile, err := c.cfg.Fs.Create(destPath)
-	if err != nil {
-		slog.Error("Failed to create target file", slog.String("target", destPath), slog.Any("error", err))
-		return err
-	}
-	defer targetFile.Close()
-
-	if _, err := io.Copy(targetFile, sourceFile); err != nil {
-		slog.Error("Failed to copy file", slog.String("source", file.Source), slog.String("target", destPath), slog.Any("error", err))
-		return err
-	}
-	slog.Debug("Copied file", slog.String("source", file.Source), slog.String("target", destPath))
-
-	if err := c.cfg.Fs.Chmod(destPath, file.SourceMode); err != nil {
-		slog.Warn("Failed to set permissions on copied file", slog.String("target", destPath), slog.Any("error", err))
-	}
-	slog.Debug("Set permissions on copied file", slog.String("target", destPath), slog.Any("mode", file.SourceMode))
-
-	return nil
+	return processFiles(c.cfg, c.paths, c.files, c.vars)
 }
